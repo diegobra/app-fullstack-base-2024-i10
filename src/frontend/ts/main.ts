f9cd687d1820f772cdd5d1827f48abdae159e19d
Diego Braga <diegobra20@gmail.com>
2024-09-28 16:10:16 -0300

    Se actualiza repositorio con pruebas realizadas en clase.

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,127 @@
+class Main implements EventListenerObject {
+    private nombre: string = "matias";
+    private users: Array<Usuario> = new Array();
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+    constructor() {
+        this.users.push(new Usuario('mramos', '123132'));
+        
+        let btn = this.recuperarElemento("btn");
+        btn.addEventListener('click', this);
+        let btnBuscar = this.recuperarElemento("btnBuscar");
+        btnBuscar.addEventListener('click', this);
+        let btnLogin = this.recuperarElemento("btnLogin");
+        btnLogin.addEventListener('click', this);
+        let btnPost = this.recuperarElemento("btnPost");
+        btnPost.addEventListener('click', this);
+    }
+    handleEvent(object: Event): void {
+        let idDelElemento = (<HTMLElement>object.target).id;
+        if (idDelElemento == 'btn') {
+            let divLogin = this.recuperarElemento("divLogin");
+            divLogin.hidden = false;
+        } else if (idDelElemento === 'btnBuscar') {
+            console.log("Buscando!")
+            this.buscarDevices();
+        } else if (idDelElemento ==='btnLogin'){
+            console.log("login")
+            let iUser = this.recuperarElemento("userName");
+            let iPass = this.recuperarElemento("userPass");
+            let usuarioNombre:string = iUser.value;
+            let usuarioPassword: string = iPass.value;
+            
+            if (usuarioNombre.length >= 4 && usuarioPassword.length >= 6) {
+                console.log("Voy al servidor... ejecuto consulta")
+                let usuario: Usuario = new Usuario(usuarioNombre, usuarioPassword);
+                let checkbox = this.recuperarElemento("cbRecor");
+                
+                console.log(usuario, checkbox.checked);
+                iUser.disabled = true;  
+                (<HTMLInputElement>object.target).disabled = true;
+                let divLogin = this.recuperarElemento("divLogin");
+                divLogin.hidden = true;
+            } else {
+                alert("El usuario o la contraseña son icorrectas");
+            }
+        } else if (idDelElemento == 'btnPost') {
+            let xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = () => {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    console.log("se ejecuto el post", xmlHttp.responseText);
+                }
+            }
+           
+            xmlHttp.open("POST", "http://localhost:8000/usuario", true);
+            
+            xmlHttp.setRequestHeader("Content-Type", "application/json"); 
+            xmlHttp.setRequestHeader("otracosa", "algo"); 
+            
+
+            let json = {name: 'mramos' };
+            xmlHttp.send(JSON.stringify(json));
+
+
+        }
+        
+    }
+
+    private buscarDevices(): void {
+        let xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = () => {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    let ul = this.recuperarElemento("list")
+                    let listaDevices: string = '';
+                   
+                    let lista: Array<Device> = JSON.parse(xmlHttp.responseText);
+        
+                    for (let item of lista) {
+                        listaDevices += `
+                        <li class="collection-item avatar">
+                        <img src="./static/images/lightbulb.png" alt="" class="circle">
+                        <span class="title">${item.name}</span>
+                        <p>${item.description} 
+                        </p>
+                        <a href="#!" class="secondary-content">
+                          <div class="switch">
+                              <label>
+                                Off`;
+                        if (item.state) {
+                            listaDevices +='<input type="checkbox" checked>'
+                        } else {
+                            listaDevices +='<input type="checkbox">'
+                        }
+                        listaDevices += `      
+                                <span class="lever"></span>
+                                On
+                              </label>
+                            </div>
+                      </a>
+                      </li>`
+                     
+                        
+                    }
+                    ul.innerHTML = listaDevices;
+                } else {
+                    alert("ERROR en la consulta");
+                }
+            }
+            
+        }
+
+        xmlHttp.open("GET", "http://localhost:8000/devices", true);
+
+        xmlHttp.send();
+
+        
+    }
+
+    private recuperarElemento(id: string):HTMLInputElement {
+        return <HTMLInputElement>document.getElementById(id);
+    }
 }
+window.addEventListener('load', () => {
+    
+    let main: Main = new Main();
+    
+});
