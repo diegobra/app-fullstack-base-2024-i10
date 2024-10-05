a472875f65c449a4328c50f3976fec32850177c6
Diego Braga <diegobra20@gmail.com>
2024-10-05 15:10:53 -0300

    Corrección de sql.

--- db/dumps/smart_home.sql
+++ db/dumps/smart_home.sql
@@ -37,30 +37,17 @@ CREATE TABLE `Devices` (
   `name` varchar(64) NOT NULL,
   `description` varchar(128) NOT NULL,
   `state` decimal(2,1) NOT NULL,
-  `typeId` int(11) NOT NULL
+  `type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Devices`
---
-
-INSERT INTO `Devices` (`id`, `name`, `description`, `state`, `typeId`) VALUES
-(1, 'Lampara 1', 'Luz living', 1, 1),
-(2, 'Lampara 2', 'Luz cocina', 0, 1),
-(3, 'Velador', 'Velador living', 1, 1),
-(4, 'Persiana 1', 'Persiana living', 1, 2),
-(5, 'Persiana 2', 'Persiana de la cocina', 1, 2),
-(6, 'Persiana 3', 'Persiana balcon', 0, 2);
-
-
---
--- Table structure for table `Devices`
+-- Table structure for table `DevicesTypes`
 --
 
 CREATE TABLE `DevicesTypes` (
   `id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
-  `icon_name` varchar(100) NOT NULL
+  `icon_name` varchar(100) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
